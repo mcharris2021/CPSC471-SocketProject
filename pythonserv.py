333b7e0f8e3e839f59176fcd87d804d3d258b732
Michael Carey <mpcarey@csu.fullerton.edu>
2023-04-20 09:30:59 -0700

    Updated system call functionality

--- pythonserv.py
+++ pythonserv.py
@@ -1,16 +1,27 @@
-import socket
+import socket, sys
 
-HOST = "0.0.0.0"
-PORT = 6000
+# Default hostname
+HOST = "localhost"
+# Default FTP port
+PORT = 21
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print("Connected by {addr}")
+
+if len(sys.argv) < 2:
+    print("Correct format: python " + sys.argv[0] + " <port number>\n")
+else:
+    PORT = int(sys.argv[1])
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen(5)
         while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+            print("Waiting for connections...")
+            conn, addr = s.accept()
+            with conn:
+                print("Connected by", addr[0])
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    conn.sendall(data)
+            conn.close()
+            break
