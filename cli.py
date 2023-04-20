8114cdac5c491bfe311c64e2178e22a032cc0f65
Michael Carey <mpcarey@csu.fullerton.edu>
2023-04-20 09:30:45 -0700

    Updated system call functionality

--- cli.py
+++ cli.py
@@ -1,11 +1,22 @@
-import socket
+import socket, sys, os
 
-HOST = "0.0.0.0"
-PORT = 6000
+# Default IP address
+HOST = "local host"
+# Default FTP port number
+PORT = 21
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.sendall(b"Hello world")
-    data = s.recv(1024)
+# Recieve Hostname and Port number from user when
+# executing the script
+if len(sys.argv) < 2:
+    print("Correct format: python " + sys.argv[0] + " <server hostname> <server port>\n")
+else:
+    HOST = sys.argv[1]
+    PORT = int(sys.argv[2])
+    
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as c:
+        c.connect((HOST, PORT))
+        c.sendall(b"Hello world")
+        data = c.recv(1024)
 
-print(f"Received {data!r}")
+    print(f"Received {data!r}")
+    c.close()
