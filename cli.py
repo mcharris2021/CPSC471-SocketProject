e690c78ab739a0bef13b37b906331688ac149234
Michael Carey <mpcarey@csu.fullerton.edu>
2023-04-22 11:06:21 -0700

    Added data connection functionality

--- cli.py
+++ cli.py
@@ -13,32 +13,46 @@ def main():
         HOST = sys.argv[1]
         PORT = int(sys.argv[2])
         # call the control connection function
-        control(HOST, PORT)
+        controlCONN(HOST, PORT)
 
 #Control connection function
-def control(HOST, PORT):
+def controlCONN(HOST, PORT):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         print(f"[+] Connecting to {HOST}:{PORT}")
         s.connect((HOST, PORT))
         print("[+] Connected.")
-        uploadFile(s)
+        dataCONN(HOST, s)
     s.close()
 
+#Data Connection Function
+def dataCONN(HOST, s):
+    dport = s.recv(4096).decode()
+    dport = int(dport)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as d:
+        print(f"[+] Establishing data connecting to {HOST}:{dport}")
+        d.connect((HOST, dport))
+        print("[+] Connected.")
+        uploadFile(d)
+    d.close()
+
 #Test function to test transfering file to server
-def uploadFile(s):
+#creating the download function should be more or less the same as uploading,
+#just reverse the client and server versions of the functions for upload (I assume)
+def uploadFile(d):
     BUFFER_SIZE = 4096
     SEPARATOR = "<SEPARATOR>"
-    #name of local text file on my computer
+    #name of local text file on my computer, this is just for test purposes.
+    #for the end product, the user would specify the name of the file to transfer
     fileName = "test1.txt"
     fileSize = os.path.getsize(fileName)
-    s.send(f"{fileName}{SEPARATOR}{fileSize}".encode())
+    d.send(f"{fileName}{SEPARATOR}{fileSize}".encode())
     
     with open(fileName, "rb") as f:
         while True:
             bytes_read = f.read(BUFFER_SIZE)
             if not bytes_read:
                 break
-            s.sendall(bytes_read)
+            d.sendall(bytes_read)
 
     print("Sent", fileSize, "bytes.")
 
--- pythonserv.py
+++ pythonserv.py
@@ -5,39 +5,91 @@ HOST = "localhost"
 # Default FTP port
 PORT = 21
 
+#main function, is called at end of program
 def main():
     if len(sys.argv) < 2:
         print("Correct format: python " + sys.argv[0] + " <port number>\n")
     else:
         PORT = int(sys.argv[1])
-        control(HOST, PORT)
+        controlCONN(HOST, PORT)
 
-def control(HOST, PORT):
+#Function for handling the main connection between client and server
+def controlCONN(HOST, PORT):
+    #establish the socket
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        #bind the socket to the server ip and port number
         s.bind((HOST, PORT))
+        #listen for connections, the number I believe handles concurrent connections
         s.listen(5)
         while True:
             print(f"[*] Listening as {HOST}:{PORT}")
+            #accept connection from client, get the connection and address from client
             conn, addr = s.accept()
+            #for test purposes, this leads directly into establishing a data connection for
+            #testing file transfers. For our end product, after establishing a connection
+            #with the client, the server would wait for commands from the client (ls, get, put)
+            #before doing anything else
             with conn:
                 print(f"[+] {addr} is connected.")
-                fileUpload(conn)
+                #call the data connection function
+                dataCONN(conn)
+            #close main connection. Once again, for our end product, the server would keep the
+            #connection open so the client can run more commands, until it gets a command from
+            #the client to close the connect, i.e. 'quit'
             conn.close()
             print(f"[-] {addr} is disconnected.")
             break
 
+#Function for handling temporary connections for file transfers
+def dataCONN(conn):
+    #create socket
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as d:
+        #make it an ephemeral port, i.e. give it a random port number
+        d.bind(('', 0))
+        #store that random port number in a variable
+        ephem = d.getsockname()[1]
+        #send that port number to the client so it know which port to connect to
+        conn.send(f"{ephem}".encode())
+        #listen for the connection from the client
+        d.listen(1)
+        #most likely don't need this while loop, but was lazy and kept it in
+        #from when I copied my code from the control connection
+        while True:
+            print(f"[*] Awaiting data connection as {HOST}:{ephem}")
+            #establish connection
+            dconn, addr = d.accept()
+            #do the task the connection was established for, i.e. either file
+            #upload or download
+            with dconn:
+                print(f"[+] data connection established with {addr}")
+                fileUpload(dconn)
+            #close connection, as this is a temporary connection, after the task
+            #is complete automatically close the connection
+            dconn.close()
+            print(f"[-] data connection closed for {addr}")
+            break
+
 #Test function to test receiving file from client
 def fileUpload(conn):
-	BUFFER_SIZE = 4096
-	SEPARATOR = "<SEPARATOR>"
-	rcvTemp = conn.recv(BUFFER_SIZE).decode()
-	filename, filesize = rcvTemp.split(SEPARATOR)
-	filename = os.path.basename(filename)
-	with open(filename, "wb") as f:
-		while True:
-			bytes_read = conn.recv(BUFFER_SIZE)
-			if not bytes_read:
-				break
-			f.write(bytes_read)
-	print(f"[+] File {filename} received")
+    #buffer size for recieving data from client
+    BUFFER_SIZE = 4096
+    #a delimiter for when the client sends the file name / size, The way I coded it
+    # the client sends both information at once, so need the separator for separating them
+    SEPARATOR = "<SEPARATOR>"
+    #recieve file information (which client sends first after connection is established)
+    rcvTemp = conn.recv(BUFFER_SIZE).decode()
+    #separate the raw data into its apporiate parts
+    filename, filesize = rcvTemp.split(SEPARATOR)
+    #remove a path from the filename if it was sent with one, so we're left with just
+    #the name of the file
+    filename = os.path.basename(filename)
+    #this handles transfering of the file
+    with open(filename, "wb") as f:
+        while True:
+            bytes_read = conn.recv(BUFFER_SIZE)
+            if not bytes_read:
+                break
+            f.write(bytes_read)
+    print(f"[+] File {filename} received")
+
 main()
